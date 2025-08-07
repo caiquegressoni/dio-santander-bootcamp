c45b87ef851731ec788a4786b345c74fb44bc5da
caiquegressoni <caiquegressoni@gmail.com>
2025-08-07 14:40:10 -0300

    Atualizado a aplicacao

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,14 +3,36 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 
-    private int number; //Numero da conta
-    private String agency; //Numero da agencia
-    private String client; //Nome do cliente
-    private double balance; //Saldo em conta
+    public String agency;
     public static void main(String[] args) throws Exception {
         var sc = new Scanner(System.in);
-        
+
         System.out.println("=====#Conta do Banco#=====");
 
+        System.out.println("Digite o número da conta: ");
+        var number = sc.nextInt();
+
+        String agency;
+        do {
+            System.out.println("Digite o número da agência (formato 000-1XX): ");
+            agency = sc.next();
+
+        }while(!ValidateAgency(agency));
+
+        System.out.println("Digite o nome do cliente: ");
+        var client = sc.next();
+
+        System.out.println("Digite o saldo da conta: ");
+        var balance = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.println("=====#Dados da Conta#=====");
+        
+        System.out.print("Olá " + client + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + number + " e seu saldo " + balance + " já está disponível para saque.");
     }
+
+    public static boolean ValidateAgency(String agency){
+        return agency.matches("\\d{3}-1\\d");
+    }
+
 }
