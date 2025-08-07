3cce8ec16ff675981cb1ff27f771e4b590a865f4
caiquegressoni <caiquegressoni@gmail.com>
2025-08-06 21:11:30 -0300

    Atualizando

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,16 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+    private int number; //Numero da conta
+    private String agency; //Numero da agencia
+    private String client; //Nome do cliente
+    private double balance; //Saldo em conta
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var sc = new Scanner(System.in);
+        
+        System.out.println("=====#Conta do Banco#=====");
+
     }
 }
